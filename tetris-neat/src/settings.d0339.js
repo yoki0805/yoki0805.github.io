c6fa5901574da466dbff487c76ad2df23dfe7712
yoki0805 <archi.crow@gmail.com>
2021-06-18 20:58:33 +0800

    Delete tetris-neat directory

--- tetris-neat/src/settings.d0339.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["Default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/TetrisTest.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"620a7",main:"ed412"}};
