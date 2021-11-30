0600dfa07e4902edebeab89414213dc425225f01
yoki0805 <archi.crow@gmail.com>
2021-11-30 17:39:17 +0800

    tetris-neat
    AI plays Tetris.

---
+++ tetris-neat/src/settings.da63d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["Default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/TetrisTest.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"468ae",main:"806e2"}};
